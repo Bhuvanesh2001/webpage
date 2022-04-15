53a3bd9001a4e655cde08d411a9662f37b68504a
Bhuvanesh2001 <bhuvaneshsai24@gmail.com>
2022-04-15 17:16:13 +0530

    boiler plate is created for app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      gngujgjgmjuyhkiykuy
+      {/* call your navbar component here */}
+
+      {/* add 'Why Study in the USA?' section here...! */}
+
+      {/* add Why choose the USA to accomplish your education goals? section here...! */}
+
+      {/* add More stats on the USA and its education section here ....! */}
+
+      {/* add Eligibility Requirements here ....! */}
+
+      {/* add  Ubergrads admitted into universities of USA section here....! */}
+
+      {/* add For all those SMART Ubergrads who would let the EXPERTS do what they do BEST! section here...! */}
+
+      {/* add footer section here....! */}
+
     </div>
   );
 }
