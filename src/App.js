c2e98ee69657616947c236c3b9338baa0058ce0a
Bhuvanesh2001 <bhuvaneshsai24@gmail.com>
2022-04-15 15:36:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      gngujgjgmjuyhkiykuy
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/eligibility requirements/EligibilityRequirements.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EligibilityRequirements() {
+  return (
+    <div>EligibilityRequirements</div>
+  )
+}
